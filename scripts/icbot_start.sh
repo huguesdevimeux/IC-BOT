e7d2ddd02bf2e6b6660a0a719d589d626c4829a1
Hugues Devimeux <thespourieye@gmail.com>
2021-07-29 12:01:26 +0200

    CD

--- scripts/icbot_start.sh
+++ scripts/icbot_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-screen -dmS icbot bash -c 'cd /home/ubuntu/icbot && source ./env/bin/activate &> icbot.log && python src/main.py &> icbot.log;'
+screen -dmS icbot bash -c 'cd /home/ubuntu/icbot && source /home/ubuntu/icbot/env/bin/activate &> icbot.log && python src/main.py &> icbot.log;'
