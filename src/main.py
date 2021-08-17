4dfa663204508a9ec8ac2e3a657983c9355066c3
Hugues Devimeux <thespourieye@gmail.com>
2021-08-17 20:28:00 +0200

    Added check for .env.secret

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
+from pathlib import Path
+
 from ICBOT import ICBOT
 from dotenv import dotenv_values
-
-TOKEN = dotenv_values(".env.secret")["TOKEN"]
+env_path = Path(__file__).parents[1] / ".env.secret"
+TOKEN = dotenv_values(env_path, verbose=True)["TOKEN"]
 ICBOT().run(TOKEN)
